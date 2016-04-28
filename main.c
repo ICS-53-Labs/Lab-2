8dea91c9a56ae045c99281f2fd7b5fc5616573bd
Joseph <josephpkc@gmail.com>
2016-04-27 22:49:55 -0700

    Main
    Created main
    -Created eval with fork and exec
    -Created and installed signal handler
    -Parseline, builtin
    TODO: Test builtin, parseline, eval, everything
    TODO: Terminate background processes with QUIT
    TODO: Other stuff?

--- main.c
+++ main.c
@@ -1,22 +1,127 @@
-/* Things needed:
-Input Reader
-Whitespace Ignores
-Buffer Increases
-Input Parser
-Argument Grabbing
-Input Validator
-Input Categorizer
-Built in Executor
-General Executor
-Forking
-Executing
-Background Checks
-Child Reaping
-SIGCHILD Handler
-Signal Installing
-Foreground Waits
-WNOHANG Reaping
-*/
+//Change name of this file to: Lab2_ID1_ID2.c"
+//"lastname, firstname: studentid"
+//My name
+//Alex's name
+ 
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <string.h>
+ #include <signal.h>
+
+ 
+ #define MAX_CMD_LINE 80
+ #define MAX_ARGS 5
+ 
+ #define QUIT "quit"
+ 
+ //Function Prototypes
+ void eval (char* cmdline);
+ int parseLine (char* cmdline, char* argv[MAX_ARGS]);
+ int builtInCommand (char* argv[MAX_ARGS]);
+ void signalHandler (int sig);
+ 
+ int main (void) {
+	 printf ("This is the process id: %d\n",getpid ());
+	 char cmdline[MAX_CMD_LINE];
+	 
+	 signal (SIGCHLD,&signalHandler);
+	 while (1) {
+		 printf ("Prompt> ");
+		 if (!fgets (cmdline,MAX_CMD_LINE,stdin)) {
+			 printf ("Fgets error\n");
+		 }
+		 if (feof (stdin)) {
+			 exit (0);
+		 }
+		 printf ("%s\n",cmdline);
+		 char* argv [MAX_ARGS];
+		 int i = parseLine(cmdline,argv);
+		 printf ("Parse: %d\n",i);
+		 //eval (cmdline);
+	 }
+	 return 0;
+ }
+ 
+ void eval (char* cmdline) {
+	 char* argv [MAX_ARGS];
+	 int bg;
+	 pid_t pid; //Maybe array
+	 
+	 bg = parseLine (cmdline, argv);
+	 if (!builtInCommand (argv)) {
+		 if ((pid = fork ()) == 0) {
+			 if (execv (argv[0],argv) < 0) {
+				 printf ("%s: Command not found.\n",argv[0]);
+				 exit (0);
+			 }
+		 }
+	 }
+	 if (!bg) {
+		 int status;
+		 if (waitpid (pid,&status,0) < 0) {
+			 //unix_error ("waitfg: waitpid error");
+		 }
+	 }
+	 else {
+		 printf ("%d %s",pid,cmdline);
+	 }
+ }
+ 
+ int parseLine (char* cmdline, char* argv[MAX_ARGS]) {
+	 //Parse into tokens
+	 const char delim[3] = " \t";
+	 int i = 0;
+	 //argv is always ended with a NULL
+	 argv[i] = strtok (cmdline,delim);
+	 printf ("Token @ %d: %s\n",i,argv[i]);
+	 while (argv[i] != NULL) {
+		 if (i + 1 >= MAX_ARGS - 1) {
+			 printf ("%d >= %d\n",i + 1,MAX_ARGS - 1);
+			 argv[i] = NULL;
+			 break;
+		 }
+		 argv[++i] = strtok (NULL,delim);
+		 printf ("Token @ %d: %s\n",i,argv[i]);
+	 }
+	 //Check last char for &: ... Arg& or ... Arg &
+	 char* last = argv[i - 1];
+	 printf ("Last %s\n",last);
+	 i = 0;
+	 while (last[i] != '\0') {
+		 ++i;
+	 }
+	 printf ("Last C:%c ''\n",last[i - 2]);
+	 if (last[i - 2] == '&') {
+		 return 1;
+	 }
+	 else {
+		 return 0;
+	 }
+ }
+ 
+ int builtInCommand (char* argv[MAX_ARGS]) {
+	 if (!strcmp (argv[0], QUIT)) {
+		 //Terminate and reap background processes OR 
+		 //do not quit until all background processes are done
+		 exit (0);
+	 }
+	 else {
+		 return 0;
+	 }
+ }
+ 
+ void signalHandler (int sig) {
+	 //safe_printf ("Handling signal %d\n",sig);
+	 int status;
+	 int res = waitpid (-1,&status,WNOHANG);
+	 if (res < 0) {
+		 //safe_printf ("waitpid error\n");
+	 }
+	 else {
+		 
+	 }
+ }
+ 
 /* Process
 INITIALIZE:
 Install Signal Handler
@@ -59,4 +164,3 @@ Print after killing, reaping
 Print after forking, exec
 */
 
-#include <std
